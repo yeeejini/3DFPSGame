206c17e60daa84bc2182bcd65c5c452fdfcd8788
yeeejini <rkdska231027@gmail.com>
2024-02-19 17:49:58 +0900

    드럼통 터트리기

--- Assets/02.Scripts/Environment/Drum.cs
+++ Assets/02.Scripts/Environment/Drum.cs
@@ -5,13 +5,51 @@ using UnityEngine;
 public class Drum : MonoBehaviour, IHitable
 {
     private int _healthCount = 0;
-   
+
+    public GameObject DrumEffectPrefabs;
+
+    Rigidbody rigidbody;
+    public float Power = 20f;
+
+    public float ExplosionRadius = 3f;
+    public int Damage = 70;
+
+    public float EXPLOSION_TIME = 3f;
+
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
     public void Hit(int damage)
     {
         _healthCount += 1;
         if (_healthCount >= 3) 
         {
-            Destroy(gameObject);
+            StartCoroutine(ExplosionDrum());
         }
+    }
+    public IEnumerator ExplosionDrum() 
+    {
+        rigidbody.AddForce(transform.up * Power, ForceMode.Impulse);
+        rigidbody.AddTorque(new Vector3(1, 0, 1) * Power / 2f);
+
+        yield return new WaitForSeconds(EXPLOSION_TIME);
+
+        GameObject drumfx = Instantiate(DrumEffectPrefabs);
+        drumfx.transform.position = this.transform.position;
+
+        int findlayer = LayerMask.GetMask("Monster") | LayerMask.GetMask("Player");
+        Collider[] colliders = Physics.OverlapSphere(transform.position, ExplosionRadius, findlayer);
+
+        foreach (Collider collider in colliders)
+        {
+            IHitable hitable = collider.GetComponent<IHitable>();
+            if (hitable != null)
+            {
+                hitable.Hit(Damage);
+            }
+        }
+
+        Destroy(gameObject);
     }
 }
